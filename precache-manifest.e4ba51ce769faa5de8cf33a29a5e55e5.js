0df250e68cfc979a90959c40bcde0420cfaa8cfa
Gilang Al Khansa <gilang.alkhansa@rea-group.com>
2020-02-05 06:55:42 +0700

    Updates

---
+++ precache-manifest.e4ba51ce769faa5de8cf33a29a5e55e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41cb9e090bba773a4d3700d963e2d3e6",
+    "url": "/fabelio-furniture/index.html"
+  },
+  {
+    "revision": "225b8b8e0516887e1954",
+    "url": "/fabelio-furniture/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9a6d5d137c694ea98de2",
+    "url": "/fabelio-furniture/static/js/2.d850dd4d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/fabelio-furniture/static/js/2.d850dd4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225b8b8e0516887e1954",
+    "url": "/fabelio-furniture/static/js/main.ccc8f8fe.chunk.js"
+  },
+  {
+    "revision": "7725a80e93c0c490399a",
+    "url": "/fabelio-furniture/static/js/runtime-main.2d22d643.js"
+  }
+]);
